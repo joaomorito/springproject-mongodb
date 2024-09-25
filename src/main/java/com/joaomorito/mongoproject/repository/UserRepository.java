b2a35033008213e66dfe750524634bc037718e5f
joaomorito <joaomorito04@gmail.com>
2024-09-25 10:24:10 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/joaomorito/mongoproject/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.joaomorito.mongoproject.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joaomorito.mongoproject.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
