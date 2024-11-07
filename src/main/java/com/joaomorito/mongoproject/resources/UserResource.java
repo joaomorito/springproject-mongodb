9b3ed1b36097dbddbf3a6c4ac547f299fc2b3ac1
joaomorito <joaomorito04@gmail.com>
2024-11-07 13:42:16 -0300

    Endpoint para retornas os posts de um usuario

--- src/main/java/com/joaomorito/mongoproject/resources/UserResource.java
+++ src/main/java/com/joaomorito/mongoproject/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaomorito.mongoproject.domain.Post;
 import com.joaomorito.mongoproject.domain.User;
 import com.joaomorito.mongoproject.dto.UserDTO;
 import com.joaomorito.mongoproject.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
