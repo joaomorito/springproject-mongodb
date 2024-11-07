7b53a5f31c6bd3437d23e81c211655a699eb0b48
joaomorito <joaomorito04@gmail.com>
2024-11-07 14:05:49 -0300

    Obtendo post por id

---
+++ src/main/java/com/joaomorito/mongoproject/services/PostService.java
@@ -0,0 +1,22 @@
+package com.joaomorito.mongoproject.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaomorito.mongoproject.domain.Post;
+import com.joaomorito.mongoproject.repository.PostRepository;
+import com.joaomorito.mongoproject.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
